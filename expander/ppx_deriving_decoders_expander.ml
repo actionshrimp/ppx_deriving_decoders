85947760cb94bef22a048e922c904551dae83bfd
Dave Aitken <dave.aitken@gmail.com>
2019-04-27 16:50:09 +0100

    decoder module gen progress and metaquot usage

--- expander/ppx_deriving_decoders_expander.ml
+++ expander/ppx_deriving_decoders_expander.ml
@@ -1,8 +1,36 @@
 module T = Ppxlib.Ast_builder.Default
 
-let str_type_decl ~loc ~path:_ (_rf, _tds) =
-  [T.pstr_value ~loc Nonrecursive
-    [{ pvb_pat = T.pvar ~loc "hello"
-     ; pvb_expr = T.estring ~loc "world"; pvb_attributes=[]; pvb_loc=loc
-     }]
-  ]
+let of_td ~loc (td: Ppxlib.type_declaration) =
+  let n = td.ptype_name.txt in
+  match td.ptype_kind with
+  | Ppxlib.Ptype_record _r ->
+    [T.pstr_module ~loc
+       { pmb_name = {txt = (Printf.sprintf "Decode_%s" n); loc }
+       ; pmb_expr =
+           T.pmod_functor ~loc {txt = "D"; loc }
+             (Some
+                (T.pmty_ident ~loc
+                   { txt =
+                       Longident.Ldot
+                         (Longident.Ldot (Longident.Lident "Decoders", "Decode"), "S")
+                   ; loc
+                   }))
+             (let f_name = T.pvar ~loc (Printf.sprintf "decode_%s" n) in
+              let f_type =
+                T.ptyp_constr ~loc {txt = (Longident.Ldot (Longident.Lident "D", "decoder")); loc } [
+                  T.ptyp_constr ~loc {txt = (Longident.Lident n); loc } []
+                ] in
+                T.pmod_structure ~loc
+                [[%stri let [%p f_name] : [%t f_type] = 3]]
+             )
+       ; pmb_attributes = []
+       ; pmb_loc = loc
+       }
+    ]
+  | _ ->
+    failwith (Printf.sprintf "Unsupported type for deriving decoders: %s" n)
+
+let str_type_decl ~loc ~path:_ (_rf, tds) =
+  tds
+  |> List.map (of_td ~loc)
+  |> List.concat
